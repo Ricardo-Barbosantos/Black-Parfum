cdef6f27f885c5a7d514ddd4167911e8602e207f
Ricardo-Barbosantos <ricardo.barbosanto@gmail.com>
2026-05-26 10:29:42 -0300

    Revert "Add decant combo checkout system"
    This reverts commit 0d7f2c244acb6a23e10f09b513d87f78862fc02e.

--- src/components/store/DecantBadge.js
+++
@@ -1,8 +0,0 @@
-export default function DecantBadge({ decant }) {
-  return (
-    <div className="rounded-md border border-white/10 bg-black/30 p-3">
-      <p className="font-semibold text-white">{decant.name}</p>
-      <p className="mt-1 text-xs uppercase tracking-[0.16em] text-gold">{decant.ml}ml</p>
-    </div>
-  );
-}
